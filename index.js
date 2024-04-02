40ba92bd7ef642b80c5d69be364685480261723d
KaziAbuZilani <zilanik53@gmail.com>
2024-04-02 14:59:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
     // res.end();
 
     let output = "url.parse(req.url, true).query";
-    res.write(output);
+    res.write('LLE');
     res.end();
     
 
