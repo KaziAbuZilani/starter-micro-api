77cea84edf0bead37847b8a11de29c02ea23b4b4
KaziAbuZilani <zilanik53@gmail.com>
2024-04-02 15:09:02 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    // console.log(`Just got a request at ${req.url}!`)
-    // res.write('On');
-    // res.end();
+const http = require("http");
+const url = require("url");
 
-    let output = url.parse(req.url, true).query;
-    res.write(output);
-    res.end();
-    
+const port = 3000;
 
-}).listen(process.env.PORT || 3000);
+let output = "OFF";
+
+const server = http.createServer((req, res) => {
+  var q = url.parse(req.url, true);
+  const path = q.pathname;
+  const { status } = q.query;
+
+  if (path == "/read") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(output);
+  } else if (path == "/write") {
+    output = status;
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Status set as: " + output);
+  }
+});
+
+server.listen(port, () => {
+  console.log(Server running at http://localhost:${port}/);
+});
