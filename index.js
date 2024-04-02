1a0bd08ee63b64fde22a2c8dd9bb5735f856bea6
KaziAbuZilani <zilanik53@gmail.com>
2024-04-02 14:36:30 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('On');
-    res.end();
+    // console.log(`Just got a request at ${req.url}!`)
+    // res.write('On');
+    // res.end();
+
+    let output = "OFF";
+    if (req.url === '/get') {
+      res.write(output);
+      res.end();
+    } else if (req.url === '/write') {
+      const status = url.parse(req.url, true).query;
+      output = status
+      res.write("Status set as" + output);
+      res.end();
+
 }).listen(process.env.PORT || 3000);
